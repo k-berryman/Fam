778c6afe6511085ad284dcd78650b1ae660f693b
Kaitlin Berryman <kaitlinberryman@gmail.com>
2022-05-30 16:19:03 -0400

    Connect messaging to Stream Chat API

--- pages/messaging.tsx
+++ pages/messaging.tsx
@@ -1,25 +1,43 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
 import Header from "../components/Header";
-import Activity from "../components/Activity";
-import Modal from "../components/Modal";
-import Chat from "../components/Chat";
 
+import 'stream-chat-react/dist/css/index.css';
 
-const Home: NextPage = () => {
-  return (
-    <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
-      <Head>
-        <title>Fam WebApp</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const chatClient = StreamChat.getInstance('dz5f4d5kzrue');
+const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoibGl2ZWx5LW1lYWRvdy0zIiwiZXhwIjoxNjUzOTU5MTg1fQ.fEuH5S3pLkaggHjzdfsbSiNw2jWjFAQz50q3UPl81JI';
 
-      <Header />
-      <h1>Messaging</h1>
-      <Chat />
-    </div>
-  )
-}
+chatClient.connectUser(
+  {
+    id: 'lively-meadow-3',
+    name: 'lively',
+    image: 'https://www.designfreelogoonline.com/wp-content/uploads/2019/02/00580-Family-02.png',
+  },
+  userToken,
+);
 
-export default Home
+const channel = chatClient.channel('messaging', 'custom_channel_id', {
+  // add as many custom fields as you'd like
+  image: 'https://www.drupal.org/files/project-images/react.png',
+  name: 'My Family',
+  members: ['lively-meadow-3'],
+});
+
+const App = () => (
+  <div>
+    <Header />
+    <Chat client={chatClient} theme='messaging light'>
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  </div>
+);
+
+export default App;
